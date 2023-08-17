0b403f81892c5e0ad417502bf4afa6bb98b347c7
Betty <bperezatencio@gmail.com>
2023-08-17 18:42:13 -0300

    ReadonlyArray

--- src/app/products/product.dto.ts
+++ src/app/products/product.dto.ts
@@ -15,6 +15,8 @@ export interface UpdateProductDto extends Partial<CreateProductDto> {};
 
 type example2 = Required<Product>;              //Contrario de Partial, que todo sea requerido
 
-export interface FindProductDto extends Readonly<Partial<Product>> {};
+export interface FindProductDto extends Readonly<Partial<Omit<Product, 'tags'>>> {   //Omitir tags, y asignarle nuevo tipado
+  readonly tags: ReadonlyArray<string>;
+};
 
 type example3 = Readonly<Product>;
